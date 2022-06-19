d1d8a2ac162d5a4565444264b8b5b2101ad9149b
Alan Smith <alan@two-cats.com>
2022-06-19 12:33:30 +0100

    Add project files.

---
+++ CtMBannerScene.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace advent
+{
+    public class CtmBannerScene : ISpecialScene
+    {
+        public bool IsActive { get; private set; }
+
+        public bool HidesTime { get; private set; }
+
+        public bool RainbowSnow => false;
+        public string Name => "CTM Banner";
+
+        private TimeSpan elapsedThisScene;
+        private Image<Rgba32> ctmBanner;
+        private bool foundImage;
+
+        private static TimeSpan sceneDuration = TimeSpan.FromSeconds(15);
+        private const int StartXOffset = 62;
+        private const int TotalMovementDistance = 243;
+        private static TimeSpan timeToMove = TimeSpan.FromSeconds(15);
+
+        public CtmBannerScene()
+        {
+            IsActive = false;
+            HidesTime = false;
+            foundImage = false;
+
+            ctmBanner = Image.Load<Rgba32>("CtMBanner32x180.png");
+            foundImage = true;
+        }
+
+        public void Activate()
+        {
+            if (foundImage)
+            {
+                elapsedThisScene = TimeSpan.Zero;
+                IsActive = true;
+                HidesTime = true;
+            }
+        }
+
+        public void Elapsed(TimeSpan timeSpan)
+        {
+            elapsedThisScene += timeSpan;
+            if (elapsedThisScene > sceneDuration)
+            {
+                IsActive = false;
+                HidesTime = false;
+            }
+        }
+
+        public void Draw(Image<Rgba32> img)
+        {
+            var x = (elapsedThisScene.TotalMilliseconds / timeToMove.TotalMilliseconds) * TotalMovementDistance;
+            var position = new Point(StartXOffset - (int)x, 0);
+            if (IsActive)
+            {
+                img.Mutate(x => x.DrawImage(ctmBanner, position, 1f));
+                if (position.X <= -180)
+                    IsActive = false;
+            }
+        }
+    }
+}
---
+++ CtMLogoScene.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace advent
+{
+    public class CtmLogoScene : ISpecialScene
+    {
+        private Point topLeft = new Point(0, 0);
+        public bool IsActive { get; private set; }
+
+        public bool HidesTime { get; private set; }
+
+        public bool RainbowSnow => false;
+        public string Name => "CTM Logo";
+
+        private TimeSpan elapsedThisScene;
+        private Image<Rgba32> ctmLogo;
+        private bool foundImage;
+
+        private static TimeSpan sceneDuration = TimeSpan.FromSeconds(6);
+        private static TimeSpan fadeDuration = TimeSpan.FromSeconds(0.5);
+
+        public CtmLogoScene()
+        {
+            IsActive = false;
+            HidesTime = false;
+            foundImage = false;
+
+            ctmLogo = Image.Load<Rgba32>("CtMLogo32x32.png");
+            foundImage = true;
+        }
+
+        public void Activate()
+        {
+            if (foundImage)
+            {
+                elapsedThisScene = TimeSpan.Zero;
+                IsActive = true;
+                HidesTime = true;
+            }
+        }
+
+        public void Elapsed(TimeSpan timeSpan)
+        {
+            elapsedThisScene += timeSpan;
+            if (elapsedThisScene > sceneDuration)
+            {
+                IsActive = false;
+                HidesTime = false;
+            }
+        }
+
+        public void Draw(Image<Rgba32> img)
+        {
+            double fraction;
+            if (elapsedThisScene < fadeDuration)
+            {
+                fraction = elapsedThisScene.TotalMilliseconds / fadeDuration.TotalMilliseconds;
+            }
+            else if (elapsedThisScene < sceneDuration - fadeDuration)
+            {
+                fraction = 1.0;
+            }
+            else
+            {
+                fraction = (sceneDuration - elapsedThisScene).TotalMilliseconds / fadeDuration.TotalMilliseconds;
+            }
+
+            fraction = Math.Min(1.0, Math.Max(0.0, fraction));
+            if (IsActive)
+            {
+                img.Mutate(x => x.DrawImage(ctmLogo, topLeft, (float)fraction));
+            }
+        }
+    }
+}
